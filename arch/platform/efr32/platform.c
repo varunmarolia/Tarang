9b82d23f463d4097d9370932f3c0ffe1e8bfd040
varunmarolia <varunmarolia@gmail.com>
2025-02-19 01:50:15 +0100

    added guart init call

--- arch/platform/efr32/platform.c
+++ arch/platform/efr32/platform.c
@@ -1,11 +1,12 @@
 #include "platform.h"
 #include "clock.h"
-
+#include "guart.h"
 /*---------------------------------------------------------------------------*/
 void
 platform_init(void) {
   board_init();   /* this will setup Xtals */
   clock_init();   /* Intialize clock */
+  guart_init();   /* Intialize generic uart */
 }
 /*---------------------------------------------------------------------------*/
 
