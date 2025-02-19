0d88cee383f540405b1e7ff4f6e2257faf675183
varunmarolia <varunmarolia@gmail.com>
2025-02-19 01:50:57 +0100

    added verion and board name print

--- arch/platform/efr32/main.c
+++ arch/platform/efr32/main.c
@@ -1,9 +1,12 @@
 #include "platform.h"
+#include "tarang-version.h"
+#include <stdio.h>
 /*---------------------------------------------------------------------------*/
 int
 main(void) {
   platform_init();  /* this will intialize the board mcu peripherals */
   app_init();
+  printf("Running Tarang " TARANG_VERSION_STRING " on " BOARD_NAME "\n");
   /* Poll all the functions periodically. */
   while(1) {
     app_poll();
