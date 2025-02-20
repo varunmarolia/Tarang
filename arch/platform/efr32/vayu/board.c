1ba6f11464a50744060eefb0545f43a5f2f8709f
varunmarolia <varunmarolia@gmail.com>
2025-02-20 15:10:05 +0100

    added led and button gpio configuration in init func

--- arch/platform/efr32/vayu/board.c
+++ arch/platform/efr32/vayu/board.c
@@ -19,22 +19,51 @@ serial_bus_t generic_uart_bus = {
   .lock = false,
   .current_dev = NULL,
   .config = {
-    .data_in_loc = DEBUG_UART_RX_LOC,
-    .data_out_loc = DEBUG_UART_TX_LOC,
-    .cts_loc = DEBUG_UART_CTS_LOC,
-    .rts_loc = DEBUG_UART_RTS_LOC,
+    .data_in_loc = UART_MCU_RX_LOC,
+    .data_out_loc = UART_MCU_TX_LOC,
+    .cts_loc = UART_MCU_CTS_LOC,
+    .rts_loc = UART_MCU_RTS_LOC,
     .parity_mode = USART_FRAME_PARITY_NONE,
     .stop_bits = USART_FRAME_STOPBITS_ONE,
-    .uart_mode = UART_MODE_TX_RX_FLOW,
+    .uart_mode = UART_MODE_TX_RX,
     .input_handler = NULL,
     .output_handler = NULL,
     .type = BUS_TYPE_UART,
     .I2Cx = NULL,
-    .SPI_UART_USARTx = UART_USART_BASE
+    .SPI_UART_USARTx = UART_USART
   }
 };
 /*---------------------------------------------------------------------------*/
 void
-board_init() {
-    akashvani1_module_init();
+board_init(void) {
+  /* Initialize the RF module. This will select XTALs */
+  akashvani1_module_init();
+  /* Enable clock for GPIO */
+  CMU_ClockEnable(cmuClock_GPIO, true);
+  /* Configure LED_PORT pin LED_PIN (User LED) as push/pull outputs */
+  GPIO_PinModeSet(LED_SYS_GREEN_PORT,         /* Port */
+                  LED_SYS_GREEN_PIN,          /* Pin */
+                  gpioModePushPull,           /* Mode */
+                  1 );                        /* Output value */
+  GPIO_PinModeSet(LED_SYS_YELLOW_PORT,        /* Port */
+                  LED_SYS_YELLOW_PIN,         /* Pin */
+                  gpioModePushPull,           /* Mode */
+                  1 );                        /* Output value */
+  GPIO_PinModeSet(LED_MODE_GREEN_PORT,        /* Port */
+                  LED_MODE_GREEN_PIN,         /* Pin */
+                  gpioModePushPull,           /* Mode */
+                  1 );                        /* Output value */
+  GPIO_PinModeSet(LED_MODE_YELLOW_PORT,       /* Port */
+                  LED_MODE_YELLOW_PIN,        /* Pin */
+                  gpioModePushPull,           /* Mode */
+                  1 );                        /* Output value */
+  /* Initialize buttons */
+  GPIO_PinModeSet(RESET_PUSH_BUTTON_PORT, 
+                  RESET_PUSH_BUTTON_PIN, 
+                  gpioModeInput, 
+                  1);
+  GPIO_PinModeSet(MODE_PUSH_BUTTON_PORT,
+                  MODE_PUSH_BUTTON_PIN,
+                  gpioModeInput,
+                  1);
 }
