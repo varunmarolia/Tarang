bf186dd053643b6f75b5df6a2cb3fa065643333b
varunmarolia <varunmarolia@gmail.com>
2025-02-20 15:23:07 +0100

    added  SHT4x sensor data

--- apps/vayu/vayu.c
+++ apps/vayu/vayu.c
@@ -1,8 +1,14 @@
 #include "board.h"
 #include "sht4x.h"
+#include "serial-status.h"
+#include <stdio.h>
 
-uint32_t sht4x_temp_mk = 0;
-uint32_t sht4x_rh = 0;
+uint32_t temperature_mk;
+int8_t temperature_c;
+int8_t temperature_c_fraction;
+uint32_t humidity_ppm;
+uint8_t humidity_rh;
+uint8_t humidity_rh_fraction;
 /*---------------------------------------------------------------------------*/
 uint8_t 
 app_init(void) {
@@ -12,7 +18,18 @@ app_init(void) {
 /*---------------------------------------------------------------------------*/
 void
 app_poll(void) {
-  sht4x_take_single_measurement(&sht4x_temp_mk, &sht4x_rh);
+  uint8_t bus_status;
+  bus_status = sht4x_take_single_measurement(&temperature_mk, &humidity_ppm);
+  if(bus_status == BUS_OK) {
+    humidity_rh = humidity_ppm / 10000; /* convert PPM into %RH */
+    humidity_rh_fraction = (humidity_ppm % 10000) / 100;
+
+    temperature_c = (temperature_mk - 273150) / 1000;
+    temperature_c_fraction = ((temperature_mk - 273150) % 1000 / 10);
+    printf("App_poll: temperature:%02d.%02u'C humidity:%02u.%02uRH\n", temperature_c, temperature_c_fraction, humidity_rh, humidity_rh_fraction);
+  } else {
+    printf("App_poll: Failed to read measurement!!!\n");
+  }
 }
 /*---------------------------------------------------------------------------*/
 
