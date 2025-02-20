d85841a6742cef03206574ec500d96f665c279b1
varunmarolia <varunmarolia@gmail.com>
2025-02-20 15:14:55 +0100

    bug fix ! added CHIP_init for corect booting

--- arch/platform/efr32/platform.c
+++ arch/platform/efr32/platform.c
@@ -1,10 +1,13 @@
 #include "platform.h"
+#include "em_chip.h"
 #include "clock.h"
 #include "guart.h"
 /*---------------------------------------------------------------------------*/
 void
-platform_init(void) {
-  board_init();   /* this will setup Xtals */
+platform_init(void) 
+{
+  CHIP_Init();    /* Initialize the chip */
+  board_init();   /* this will setup and select 38.4MHz Xtal and configure LEDs and button ports */
   clock_init();   /* Intialize clock */
   guart_init();   /* Intialize generic uart */
 }
