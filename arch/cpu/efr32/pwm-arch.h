2ec1d4afb9e86d9b566d555bdbdd73ea0effc8e9
varunmarolia <varunmarolia@gmail.com>
2025-03-07 23:46:19 +0100

    added macro for enable disable pwm device

--- arch/cpu/efr32/pwm-arch.h
+++ arch/cpu/efr32/pwm-arch.h
@@ -2,6 +2,10 @@
 #define _PWM_ARCH_H_
 #include <stdint.h>
 #include <em_timer.h>
+
+#define PWM_DEVICE_DISABLE  0
+#define PWM_DEVICE_ENABLE   1
+
 typedef struct pwm_config {
   const uint32_t freq_hz;
   /* arch specific */
