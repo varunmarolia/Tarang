6fe749507a4178243e4b953ff1d927feb3881e7e
varunmarolia <varunmarolia@gmail.com>
2025-03-14 03:03:11 +0100

    added guart read line and watchdog clear calls

--- arch/platform/efr32/main.c
+++ arch/platform/efr32/main.c
@@ -4,10 +4,13 @@
 #include "guart.h"
 #include "board-common.h"
 #include <stdio.h>
+#include "watchdog.h"
 /*---------------------------------------------------------------------------*/
 int
 main(void) {
   ttimer_t poll_timer;
+  uint8_t guart_rx[GUART_RX_BUFFER_SIZE + 1];
+  uint8_t guart_read_bytes;
   platform_init();  /* this will intialize the board mcu peripherals */
   app_init();
   led_blink(LED_SYS_GREEN_PORT, LED_SYS_GREEN_PIN, 2, 250);
@@ -17,10 +20,17 @@ main(void) {
   timer_set(&poll_timer,  0);
   /* Poll all the functions periodically. */
   while(1) {
-    while(!timer_timedout(&poll_timer));
-    app_poll();
-    timer_set(&poll_timer, 3000);
-    led_blink(LED_SYS_YELLOW_PORT, LED_SYS_YELLOW_PIN, 1, 100);
+    guart_read_bytes = guart_read_line(guart_rx);
+    if(guart_read_bytes) {
+      guart_rx[guart_read_bytes] = '\0';
+      printf("Main: received: %s\n", guart_rx);
+    }
+    if(timer_timedout(&poll_timer)) {
+      app_poll();
+      timer_set(&poll_timer, 3000);
+      led_blink(LED_SYS_YELLOW_PORT, LED_SYS_YELLOW_PIN, 1, 100);
+    }
+    watchdog_feed();
   }
 return 0;
 }
--- tarang/dev/guart/guart.h
+++ tarang/dev/guart/guart.h
@@ -7,6 +7,7 @@
 #define GUART_MAX_STD_UART_BAUDRATE 921600          /* 921.6Kbps, Actual speed 92160 Bps */
 void guart_send_data(const uint8_t *data, uint16_t bytes);
 uint16_t guart_read_data(uint8_t *data);
+uint16_t guart_read_line(uint8_t *data);
 void guart_init(void);
 void guart_input_handler(uint8_t data);
 void guart_puts(const char *str);
