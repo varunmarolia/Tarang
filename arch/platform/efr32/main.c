290e7f5ad403e30b1d73d3b11455d6c45564b7f7
varunmarolia <varunmarolia@gmail.com>
2025-04-02 11:50:58 +0200

    Added debug macro to enable debug_uart

--- arch/platform/efr32/main.c
+++ arch/platform/efr32/main.c
@@ -8,13 +8,15 @@
 /*---------------------------------------------------------------------------*/
 int
 main(void) {
+#ifdef DEBUG
   uint8_t guart_rx[GUART_RX_BUFFER_SIZE + 1];
   uint8_t guart_read_bytes;
   extern guart_t uart_debug;
+#endif /* DEBUG */
   platform_init();  /* this will initialize the board MCU peripherals */
   app_init();
   led_sys_blink(LED_SYS_GREEN_PORT, LED_SYS_GREEN_PIN, 2, 250);
-  printf("Main: Running Tarang " TARANG_VERSION_STRING " on " BOARD_NAME "\n");
+  printf("\nMain: Running Tarang " TARANG_VERSION_STRING " on " BOARD_NAME "\n");
   printf("Main: Arch info --->\n");
   print_chip_info();
   printf("Main: VCC:%lu mV VDD:%lu mV\n", 
