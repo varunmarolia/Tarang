682472657979e99dc20e18a987748cbf71319052
varunmarolia <varunmarolia@gmail.com>
2025-05-28 23:58:14 +0200

    added pwm status

--- tarang/dev/common/pwm-dev.c
+++ tarang/dev/common/pwm-dev.c
@@ -28,29 +28,56 @@
 #include "pwm-dev.h"
 
 /*---------------------------------------------------------------------------*/
-void
+pwm_status_t
 pwm_dev_init(pwm_dev_t *dev)
 {
+  pwm_status_t status = PWM_STATUS_OK;
   if(dev != NULL && dev->config != NULL) {
-    pwm_arch_init(dev);
+    status = pwm_arch_init(dev);
+  } else {
+    return PWM_STATUS_INVALID_PARAM;
   }
-  pwm_arch_enable_device(dev, PWM_DEVICE_ENABLE);
+  if(status == PWM_STATUS_OK) {
+    status = pwm_arch_enable_device(dev, PWM_DEVICE_ENABLE);
+  }
+  return status;
 }
 /*---------------------------------------------------------------------------*/
-void
-pwm_dev_set_duty_cycle(pwm_dev_t *dev)
+pwm_status_t
+pwm_dev_set_duty_cycle(pwm_dev_t *dev, uint16_t duty_cycle_100x)
 {
   if(dev != NULL && dev->config != NULL) {
-    pwm_arch_set_duty_cycle(dev);
+    if(duty_cycle_100x > 10000) {
+      duty_cycle_100x = 10000; /* duty cycle cannot be more than 100% */
+    }
+    /* only update the pwm buffer value if it is different than the current value */
+    if(dev->duty_cycle_100x != duty_cycle_100x) {
+      dev->duty_cycle_100x = duty_cycle_100x; /* set the new duty cycle */
+      return pwm_arch_set_duty_cycle(dev);
+    } else {
+      return PWM_STATUS_OK;
+    }
+  } else {
+    return PWM_STATUS_INVALID_PARAM;
   }
+  return PWM_STATUS_OK;
 }
 /*---------------------------------------------------------------------------*/
-void
+pwm_status_t
 pwm_dev_reset(pwm_dev_t *dev) 
 {
-  pwm_arch_enable_device(dev, PWM_DEVICE_DISABLE);
+  pwm_status_t status = PWM_STATUS_OK;
+
   if(dev != NULL && dev->config != NULL) {
-    pwm_arch_reset(dev);
+    status = pwm_arch_enable_device(dev, PWM_DEVICE_DISABLE);
+    if(status != PWM_STATUS_OK) {
+      return status; /* if disabling the device fails, return error */
+    }
+    /* reset the pwm arch for the given device */
+    status = pwm_arch_reset(dev);
+  } else {
+    return PWM_STATUS_INVALID_PARAM;
   }
+  return status;
 }
 /*---------------------------------------------------------------------------*/
--- tarang/dev/common/pwm-dev.h
+++ tarang/dev/common/pwm-dev.h
@@ -5,6 +5,14 @@
 #include <stddef.h>
 #include "common-arch.h"
 #include "pwm-arch.h"
+
+typedef enum pwm_status {
+  PWM_STATUS_OK = 0,
+  PWM_STATUS_BUSY = -1,
+  PWM_STATUS_INVALID_TIMER = -2,
+  PWM_STATUS_INVALID_CHANNEL = -3,
+  PWM_STATUS_INVALID_PARAM = -4
+} pwm_status_t;
 typedef struct pwm_dev {
   const uint8_t cc_channel;               /* compare channel number, the device is on */
   volatile uint16_t duty_cycle_100x;      /* 100 x value of duty cycle in percentage. i.e 5124 is 51.24 % */
@@ -15,12 +23,12 @@ typedef struct pwm_dev {
   gpio_config_t *dev_enable;              /* device enable pin. Some devices have separate pins for power enable and PWM */
 } pwm_dev_t;
 
-void pwm_dev_init(pwm_dev_t *dev);
-void pwm_dev_set_duty_cycle(pwm_dev_t *dev);
-void pwm_dev_reset(pwm_dev_t *dev);
+pwm_status_t pwm_dev_init(pwm_dev_t *dev);
+pwm_status_t pwm_dev_set_duty_cycle(pwm_dev_t *dev, uint16_t duty_cycle_100x);
+pwm_status_t pwm_dev_reset(pwm_dev_t *dev);
 /* arch specific functions */
-void pwm_arch_init(pwm_dev_t *dev);
-void pwm_arch_reset(pwm_dev_t *dev);
-void pwm_arch_set_duty_cycle(pwm_dev_t *dev);
-void pwm_arch_enable_device(pwm_dev_t *dev, uint8_t on_off);
+pwm_status_t pwm_arch_init(pwm_dev_t *dev);
+pwm_status_t pwm_arch_reset(pwm_dev_t *dev);
+pwm_status_t pwm_arch_set_duty_cycle(pwm_dev_t *dev);
+pwm_status_t pwm_arch_enable_device(pwm_dev_t *dev, uint8_t on_off);
 #endif /* _PWM_DEV_H_ */
